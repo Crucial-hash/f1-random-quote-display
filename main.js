5a9650a5796ac3db9b312b2f55af261ce9ca45e3
Crucial-hash <yusuf10101010234@gmail.com>
2024-05-13 22:33:58 +0100

    tidy up files

--- main.js
+++ main.js
@@ -2,49 +2,42 @@ const API_ENDPOINT = "quotes.json";
 let quotes = [];
 
 const init = async () => {
-  try {
-    const jsonRes = await fetch(API_ENDPOINT);
-    quotes = await jsonRes.json();
-    setMarkup();
-  } catch (error) {
-    quotation.innerHTML = error.message;
-  }
-};
+try {
+  const jsonRes = await fetch(API_ENDPOINT);
+  quotes = await jsonRes.json();
+  setMarkup();
+} catch (error) {
+  quotation.innerHTML = error.message;
+}};
 
 const randomIndexGenerator = (len) => {
-  const array = new Uint32Array(1);
-  window.crypto.getRandomValues(array);
-  const randomIndex = array[0] % len;
-  return randomIndex;
+const array = new Uint32Array(1);
+window.crypto.getRandomValues(array);
+const randomIndex = array[0] % len;
+return randomIndex;
 };
 
 const setMarkup = () => {
-  const quoteData = quotes[randomIndexGenerator(quotes.length)];
-  const quoteText = quoteData.text || quoteData.quote; // Handle both 'text' and 'quote' properties
+const quoteData = quotes[randomIndexGenerator(quotes.length)];
+const quoteText = quoteData.text || quoteData.quote;
 
-  quotation.innerHTML = `
-        <span class="quote">${quoteText}</span>  
-        <div class="footer">
-            <span id="refresh" title="Refresh | click R">
-            </span>
-            <div class="author">
-                <span class="line"></span>&nbsp;
-                <span>${quoteData.author ? quoteData.author : "Anonymous"}</span>
-            </div>
-        </div>
-    `;
-  refresh.addEventListener("click", () => {
-    setMarkup();
-  });
-};
+quotation.innerHTML = `
+      <span class="quote">${quoteText}</span>  
+      <div class="footer">
+          <span id="refresh" title="Refresh | click R">
+          </span>
+          <div class="author">
+              <span class="line"></span>&nbsp;
+              <span>${quoteData.author ? quoteData.author : "Anonymous"}</span>
+          </div>
+      </div>
+  `;
+refresh.addEventListener("click", () => {
+  setMarkup();
+});};
 
 var button = document.getElementById("homea");
-
-// Add a click event listener to the button
 button.addEventListener("click", function() {
-  // Redirect to the desired website
   window.location.href = "http://homeassistant.local:8123/dashboard-ximi/0";
 });
-
-// Initialize the application
 init();
